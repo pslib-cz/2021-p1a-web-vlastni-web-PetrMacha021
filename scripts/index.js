18403f17a3e7751d79cd721f40efc40d0cb6cbd1
Mapetr <info@mapetr.cz>
2022-05-26 16:49:01 +0200

    first canvas

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,46 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-
-const socket = io("ws://localhost:3000");
-
-socket.on("connect", () => {
-  console.log("connected");
+document.addEventListener("DOMContentLoaded", function () {
+    let temp = new XMLHttpRequest();
+    temp.addEventListener("load", drawTemp);
+    temp.open("GET", "https://meteo.mapetr.cz/hour/temp");
+    temp.send();
 });
 
-socket.on("disconnect", () => {
-  console.log("disconnected");
-});
-
-socket.on("message", (data) => {
-  console.log(data);
-});
+function drawTemp() {
+    console.log(this.response);
+    const canvas = document.getElementById('temp');
+    const ctx = canvas.getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["bruh", "hrih"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
